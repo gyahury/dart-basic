f5d1567a580786e207e55903a9d5733795846aea
gyahury <rudgns1243@naver.com>
2024-08-15 17:58:38 +0900

    docs: 변수 내용 추가

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
 void main() {
+  // 다트 언어에서는 세미콜론 중요
   print('hello world');
+  
+  // 1. 변수만드는 두가지 방법 
+  // 관습으로 함수나 메소드 내부에 지역변수 선언할 때 var 사용
+  var name = 'gyahury';
+  name = 'rudgns';
+  
+  // class 변수나 프로퍼티 선언할 때는 타입 지정
+  String name2 = 'gyahury';
+  name = 'rudgns';
+
 }
